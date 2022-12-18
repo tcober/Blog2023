76a55668f2a0eac195eb445af7a2e8be5c9bec55
Thomas Cober <tcober5@gmail.com>
2022-12-17 19:48:27 -0600

    trying a different import method

--- storyblok/BlogCode.vue
+++ storyblok/BlogCode.vue
@@ -1,10 +1,11 @@
 <template>
   <div>
-    <pre><code :class="`language-${props.blok.language}`" v-html="props.blok.code"></code></pre>
+    <pre><code :class="`language-${props.blok.language}`" v-html="escape(props.blok.code)"></code></pre>
   </div>
 </template>
 
 <script setup>
+import escape from "lodash/escape";
 const props = defineProps({ blok: Object });
 </script>
 
