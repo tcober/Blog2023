701054087ab6b34044b8d417699f5e17eabe88a4
Thomas Cober <tcober5@gmail.com>
2023-09-10 20:47:02 -0500

    trying some image optimization settings

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -30,6 +30,16 @@ export default defineNuxtConfig({
     storyblok: {
       baseURL: "https://a-us.storyblok.com",
     },
+    format: ["webp"],
+    screens: {
+      xs: 320,
+      sm: 640,
+      md: 768,
+      lg: 1024,
+      xl: 1280,
+      xxl: 1536,
+      "2xl": 1536,
+    },
   },
   modules: [
     [
