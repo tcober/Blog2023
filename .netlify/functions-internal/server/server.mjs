858c6777538e6a2213a2ba66b484db74be947bca
Thomas Cober <tcober5@gmail.com>
2024-04-19 20:17:01 -0500

    Revert "Revert "Revert "trying something"""
    This reverts commit d421e540ba2768c66480faa82d2d8e9cae39c676.

--- .netlify/functions-internal/server/server.mjs
+++
@@ -1,8 +0,0 @@
-globalThis._importMeta_={url:import.meta.url,env:process.env};export { I as handler } from './chunks/nitro/netlify.mjs';
-import 'node:http';
-import 'node:https';
-import 'fs';
-import 'path';
-import 'node:url';
-import 'ipx';
-//# sourceMappingURL=server.mjs.map
